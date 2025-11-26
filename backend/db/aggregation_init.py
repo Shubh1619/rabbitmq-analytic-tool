0dcade279fe7a5036d00878b6f3214d9bd69cac7
Shubh1619 <ubalekartik203@gmail.com>
2025-11-26 11:40:40 +0530

    start to superset-ui updating

--- backend/db/aggregation_init.py
+++ backend/db/aggregation_init.py
@@ -2,10 +2,10 @@ import clickhouse_connect
 import os
 
 client = clickhouse_connect.get_client(
-    host=os.getenv("CLICKHOUSE_HOST", "clickhouse"),
-    port=int(os.getenv("CLICKHOUSE_PORT", 8123)),
-    username=os.getenv("CLICKHOUSE_USER", "default"),
-    password=os.getenv("CLICKHOUSE_PASSWORD", "")
+    host=os.getenv("CLICKHOUSE_HOST"),
+    port=int(os.getenv("CLICKHOUSE_PORT")),
+    username=os.getenv("CLICKHOUSE_USER"),
+    password=os.getenv("CLICKHOUSE_PASSWORD")
 )
 
 def init_aggregation():
